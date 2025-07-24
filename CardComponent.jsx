f0eb5c4507f35876629f6641eec4a786a1e80c60
priyamkk <priyamcodeforces@gmail.com>
2025-07-24 22:59:56 +0530

    Create CardComponent.jsx

---
+++ CardComponent.jsx
@@ -0,0 +1,8 @@
+<div class="border shadow-lg w-80 mx-auto p-4 rounded-lg">
+  <img class="mb-3 rounded-md" src="https://picsum.photos/id/237/200/300" alt="Card Image">
+<h3 class="font-bold mb-3">Heading</h3>
+<div class="text-gray-500 text-sm mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta exercitationem quas impedit aliquid architecto provident praesentium quo. Ut doloribus excepturi iste eveniet ipsa aut dolores aspernatur illum, culpa dolorem dignissimos?</div>
+<div class="text-center">
+  <a href="#" class="bg-blue-800 text-white px-4 py-2 inline-block rounded-full">button</a>
+</div>
+</div>
